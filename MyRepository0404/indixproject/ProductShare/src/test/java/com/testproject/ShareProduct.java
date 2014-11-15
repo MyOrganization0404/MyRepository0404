1ef481b82113d10843cbda3f1b29fc63b6ae65f4
simonnicholas0404 <simonnicholas0404@gmail.com>
2014-11-15 22:11:53 +0530

    Checked in code for product search
    Signed-off-by: simonnicholas0404 <simonnicholas0404@gmail.com>

--- MyRepository0404/indixproject/ProductShare/src/test/java/com/testproject/ShareProduct.java
+++ MyRepository0404/indixproject/ProductShare/src/test/java/com/testproject/ShareProduct.java
@@ -1,8 +1,13 @@
 package com.testproject;
 
-import org.openqa.selenium.chrome.ChromeDriver;
+/*
+ * Test to search a product and share to self account in amazon.in
+ */
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.Test;
 
@@ -12,26 +17,37 @@ public class ShareProduct {
 	@Test
 	public void shareBoseProduct() throws Exception{
 		
-		System.setProperty("webdriver.chrome.driver", "C:\\chromedriver.exe");
-		RemoteWebDriver driver=new ChromeDriver(); 
-	    driver.get("http://www.amazon.in" + "/ap/signin?_encoding=UTF8&openid.assoc_handle=inflex&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.mode=checkid_setup&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0&openid.ns.pape=http%3A%2F%2Fspecs.openid.net%2Fextensions%2Fpape%2F1.0&openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.amazon.in%2Fgp%2Fcss%2Fhomepage.html%3Fie%3DUTF8%26ref_%3Dnav_yam_ya");
+		// Instantiate FF driver
+		RemoteWebDriver driver=new FirefoxDriver(); 
+		driver.manage().window().maximize();
+		//login to amazon.in
+	    driver.get(Constants.loginUrL);
 	    driver.findElementById("ap_email").clear();
-	    driver.findElementById("ap_email").sendKeys("simonnicholas0404@gmail.com");
+	    driver.findElementById("ap_email").sendKeys(Constants.userID);
 	    driver.findElementById("ap_password").clear();
-	    driver.findElementById("ap_password").sendKeys("nicholas0404");
+	    driver.findElementById("ap_password").sendKeys(Constants.PWD);
 	    driver.findElementById("signInSubmit-input").click();
-	 
-	    driver.findElementById("signInSubmit-input").click();
+	    Thread.sleep(5000);
+	    //search for product bose
 	    driver.findElementById("twotabsearchtextbox").click();
 	    driver.findElementById("twotabsearchtextbox").clear();
-	    driver.findElementById("twotabsearchtextbox").sendKeys("bose");
-	   
-	   driver.findElementByCssSelector("input.nav-submit-input").click();
-	   driver.findElementById("result_0").click();
-	    //driver.findElementById("swfText").click();
+	    driver.findElementById("twotabsearchtextbox").sendKeys(Constants.prodSearchTxt);
+	    driver.findElementByCssSelector("input.nav-submit-input").click();
+	    
+	    // click on the first listed search
+	    List<WebElement> searchElements = driver.findElements(By.xpath("//ul[@id='s-results-list-atf']/li")); 
+	    searchElements.get(Constants.prodIndex).findElement(By.tagName("a")).click();
+	    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	    
+	    //share via email
+	    String shareUrl=driver.findElementById("swfText").getAttribute("href");
+	    driver.get(shareUrl);
+	    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 	    driver.findElementById("swfRecTextarea").clear();
-	    driver.findElementById("swfRecTextarea").sendKeys("simonnicholas0404@gmail.com");
+	    driver.findElementById("swfRecTextarea").sendKeys(Constants.userID);
 	    driver.findElementById("swfSubmitButton-announce").click();
+	    //close the driver
+	    driver.close();
 
 
 	}
