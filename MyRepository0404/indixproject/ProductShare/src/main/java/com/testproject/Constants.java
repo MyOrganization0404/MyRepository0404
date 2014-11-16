9e4a7804eff26f22bdacc22f69a857dd03646483
simonnicholas0404 <simonnicholas0404@gmail.com>
2014-11-16 15:58:58 +0530

    Moved User credentials from constants to properties files

--- MyRepository0404/indixproject/ProductShare/src/main/java/com/testproject/Constants.java
+++ MyRepository0404/indixproject/ProductShare/src/main/java/com/testproject/Constants.java
@@ -2,15 +2,11 @@ package com.testproject;
 
 public class Constants 
 {
- 
-	
+
 	public static String baseUrl="http://www.amazon.in";
 	public static String loginUrL="http://www.amazon.in" + "/ap/signin?_encoding=UTF8&openid.assoc_handle=inflex&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.mode=checkid_setup&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0&openid.ns.pape=http%3A%2F%2Fspecs.openid.net%2Fextensions%2Fpape%2F1.0&openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.amazon.in%2Fgp%2Fcss%2Fhomepage.html%3Fie%3DUTF8%26ref_%3Dnav_yam_ya";
-	public static String userID="simonnicholas0404@gmail.com";
-	public static String PWD="nicholas0404";
 	public static String prodSearchTxt="bose";
 	public static int prodIndex=0;
-	
-	
+	public static String loginMailUrL="https://accounts.google.com/ServiceLogin?service=mail&passive=true&rm=false&continue=https://mail.google.com/mail/&ss=1&scc=1&ltmpl=default&ltmplcache=2&emr=1";
 	
 }
--- MyRepository0404/indixproject/ProductShare/src/test/java/com/testproject/ShareProduct.java
+++ MyRepository0404/indixproject/ProductShare/src/test/java/com/testproject/ShareProduct.java
@@ -1,5 +1,4 @@
 package com.testproject;
-
 /*
  * Test to search a product and share to self account in amazon.in
  */
@@ -10,6 +9,10 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.Test;
+import java.io.FileInputStream;
+
+import java.util.Properties;
+
 
 public class ShareProduct {
 
@@ -17,21 +20,34 @@ public class ShareProduct {
 	@Test
 	public void shareBoseProduct() throws Exception{
 		
-		// Instantiate FF driver
-		RemoteWebDriver driver=new FirefoxDriver(); 
-		driver.manage().window().maximize();
+
+		  //Create Object of Properties Class.
+		  Properties obj = new Properties();  
+		  
+		  //Create Object of FileInputStream Class. Pass file path.
+		  FileInputStream objfile = new FileInputStream(System.getProperty("user.dir")+"\\src\\main\\java\\objects.properties");
+		  
+		  //Pass object reference obj file to load method of Properties object.
+		  obj.load(objfile);
+		  
+			// Instantiate FF driver
+			RemoteWebDriver driver=new FirefoxDriver(); 
+			driver.manage().window().maximize();
+			
 		//login to amazon.in
-	    driver.get(Constants.loginUrL);
+	
+		driver.get(Constants.loginUrL);
 	    driver.findElementById("ap_email").clear();
-	    driver.findElementById("ap_email").sendKeys(Constants.userID);
+	    driver.findElementById("ap_email").sendKeys(obj.getProperty("userID"));
 	    driver.findElementById("ap_password").clear();
-	    driver.findElementById("ap_password").sendKeys(Constants.PWD);
+	    driver.findElementById("ap_password").sendKeys(obj.getProperty("PWD"));
 	    driver.findElementById("signInSubmit-input").click();
 	    Thread.sleep(5000);
-	    //search for product bose
+	    
+	    //search for product BOSE Product
 	    driver.findElementById("twotabsearchtextbox").click();
 	    driver.findElementById("twotabsearchtextbox").clear();
-	    driver.findElementById("twotabsearchtextbox").sendKeys(Constants.prodSearchTxt);
+	    driver.findElementById("twotabsearchtextbox").sendKeys(obj.getProperty("prodSearchTxt"));
 	    driver.findElementByCssSelector("input.nav-submit-input").click();
 	    
 	    // click on the first listed search
@@ -44,11 +60,13 @@ public class ShareProduct {
 	    driver.get(shareUrl);
 	    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 	    driver.findElementById("swfRecTextarea").clear();
-	    driver.findElementById("swfRecTextarea").sendKeys(Constants.userID);
-	    driver.findElementById("swfSubmitButton-announce").click();
+	    driver.findElementById("swfRecTextarea").sendKeys(obj.getProperty("userID"));
+	    driver.findElementById("swfSubmitButton-announce").submit();
+	    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	    
 	    //close the driver
 	    driver.close();
-
-
 	}
+	
+	
 }
